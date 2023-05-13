97fcc2f25a2e3459bbc15e511418862e2159743e
Gustavo Souza <gustavo.s.silva651@gmail.com>
2023-05-13 02:28:41 -0300

    chore(server): Updating headers on API

--- web/src/Services/api.ts
+++ web/src/Services/api.ts
@@ -2,4 +2,9 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://react-esports.vercel.app/",
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  },
 })
