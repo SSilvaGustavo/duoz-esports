022f2830ff3fa74ab5c190fb718fcc4ffe40a50f
Gustavo Souza <gustavo.s.silva651@gmail.com>
2023-05-14 17:46:24 -0300

    chore(web): little updates

--- web/src/Services/api.ts
+++ web/src/Services/api.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://react-esports.vercel.app",
   headers: {
-    'Access-Control-Allow-Credentials': "true",
     'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': "true",
     'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE',
     'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   },
