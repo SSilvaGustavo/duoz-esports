d506c55db3eece5f9b52b6dc9e324d7394089b9e
Gustavo Souza <gustavo.s.silva651@gmail.com>
2023-05-17 20:40:45 -0300

    feat(web): NotFound responsive

--- web/src/pages/NotFound.tsx
+++ web/src/pages/NotFound.tsx
@@ -2,22 +2,39 @@ import { Link } from "react-router-dom";
 import valorantChar1 from "../assets/valorant-char1.gif";
 import valorantChar2 from "../assets/valorant-char2.gif";
 import { ArrowRight } from "phosphor-react";
+import { Fragment, useContext } from "react";
+import { AppContext } from "../components/Context/AppContext";
 
 export function NotFound() {
+  const { isMobile, isDesktop } = useContext(AppContext);
+
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 overflow-hidden">
       <div className="flex text-zinc-200 items-center justify-center">
         <Link to={"https://twitter.com/QuelmNossfe"} target="_blank">
-          <img src={valorantChar1} alt="" className="max-w-max cursor-pointer" />
+          <img src={valorantChar1} alt="" className={`${isDesktop ? "cursor-pointer pl-10" : "hidden"}`} />
         </Link>
         <div className="flex flex-col">
-          <div className="flex flex-col items-center mb-[12rem]">
-            <h1 className="text-[240px] font-bold">4<span className="bg-text-error-gradient bg-clip-text text-transparent">0</span>4</h1>
-            <h2 className="text-5xl font-bold tracking-wider">PAGE <span className="bg-text-error-gradient bg-clip-text text-transparent">NOT </span> FOUND</h2>
+          <div className="flex flex-col items-center mb-6 lg:mb-[12rem]">
+            <h1 className="text-[120px] font-bold md:text-[240px]">4<span className="bg-text-error-gradient bg-clip-text text-transparent">0</span>4</h1>
+            <h2 className="w-max text-4xl font-bold tracking-wider md:text-5xl">PAGE <span className="bg-text-error-gradient bg-clip-text text-transparent">NOT </span> FOUND</h2>
           </div>
+          {
+            !isDesktop ? 
+            <div className="flex">
+            <Link to={"https://twitter.com/QuelmNossfe"} target="_blank">
+              <img src={valorantChar1} alt="" className="" />
+            </Link>
+            <Link to={"https://twitter.com/QuelmNossfe"} target="_blank">
+              <img src={valorantChar2} alt="" className="" />
+            </Link>
+          </div>
+          :
+          <Fragment></Fragment>
+          }
           <div className="flex flex-col items-center gap-3 tracking-tight">
             <p className="text-3xl font-semibold">SEGUE A <span className="bg-text-error-gradient bg-clip-text text-transparent">CALL!</span></p>
-            <p className="text-lg w-max">Parece que seu duo esta camperando em algum lugar, mas não é aqui.</p>
+            <p className="text-center mb-6 lg:w-max lg:mb-0 lg:text-lg">Parece que seu duo esta camperando em algum lugar, mas não é aqui.</p>
             
           <Link to={"/"} className="flex items-center gap-3 py-3 px-4 text-white rounded bg-space-700 hover:bg-space-800 transition-all group
           hover:text-space-200">
@@ -27,7 +44,7 @@ export function NotFound() {
           </div>
         </div>
         <Link to={"https://twitter.com/QuelmNossfe"} target="_blank">
-          <img src={valorantChar2} alt="" className="max-w-max cursor-pointe" />
+          <img src={valorantChar2} alt="" className={`${isDesktop ? "cursor-pointer pr-10" : "hidden"}`} />
         </Link>
       </div>
     </div>
