c0dae86f6b6a0eb13a3d7f8a005dc4d9a6a13f2b
Gustavo Souza <gustavo.s.silva651@gmail.com>
2023-06-11 16:23:42 -0300

    test(web): Redirecting page error to custom 404

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { GameAd } from "./pages/GameAd";
-import { NotFound } from "./pages/NotFound";
+import { NotFound } from "./pages/404";
 
 export function Router() {
   return (
